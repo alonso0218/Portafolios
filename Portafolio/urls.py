de291a3e3b8ba21ab8127b487d88538c39fae342
alonso0218 <oalonso222@gmail.com>
2021-05-07 23:32:19 -0300

    modificacion url

--- Portafolio/urls.py
+++ Portafolio/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("contactoapp.urls")),
+    path('contactoapp',include("contactoapp.urls")),
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
